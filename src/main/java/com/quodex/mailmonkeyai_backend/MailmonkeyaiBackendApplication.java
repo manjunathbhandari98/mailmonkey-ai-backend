b4bf6c3fabebebcff2e3e74c0558a322fc286851
Manjunath <bhandarimanjunath98@gmail.com>
2025-12-04 18:11:19 +0530

    Initiate project

---
+++ src/main/java/com/quodex/mailmonkeyai_backend/MailmonkeyaiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.quodex.mailmonkeyai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailmonkeyaiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailmonkeyaiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quodex/mailmonkeyai_backend/MailmonkeyaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quodex.mailmonkeyai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailmonkeyaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
